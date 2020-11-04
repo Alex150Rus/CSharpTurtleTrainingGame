23bad70fe4998019ebad732944a92f028185fccc
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2020-11-04 14:47:50 +0300

    my turtle descision

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +13,32 @@ namespace TurtleTrainingGame
     {
         static void Main(string[] args)
         {
+            Turtle.Speed = 9;
+            int i = 0;
+            while(i < 4) { 
+                Turtle.Move(10);
+                Turtle.TurnRight();
+                Turtle.Move(10);
+                Turtle.TurnRight();
+                Turtle.Move(10);
+                Turtle.TurnLeft();
+                Turtle.Move(10);
+                Turtle.TurnLeft();
+                i++;
+            }
 
+            i = 0;
+
+            while (i < 2)
+            {
+                Turtle.Move(100);
+                Turtle.Turn(45);
+                Turtle.Move(100);
+                Turtle.TurnRight();
+                Turtle.Move(100);
+                Turtle.Turn(45);
+                i++;
+            }
         }
     }
 }
