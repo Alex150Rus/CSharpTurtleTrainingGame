4e330d38e38b1902c26a7ad1dc3e8c1fcc4d3378
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2020-11-04 13:43:19 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SmallBasic.Library;
+
+
+namespace TurtleTrainingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
